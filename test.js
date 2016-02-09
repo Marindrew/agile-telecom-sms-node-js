6d2b2c7d9a3fe22240235838ee8043213bbe3967
Marindrew <a@fj.by>
2016-02-09 14:49:44 +0000

    Working Agile POST HTTP Client

---
+++ test.js
@@ -0,0 +1,17 @@
+
+
+var AgileClient = require("./agileClient");
+
+
+var agileClient = new AgileClient("test", "test", function(err, result) {
+	if(!err && result && result.client) {
+		result.client.sendSms({
+			fromPhoneNumber: "Hiboo",
+			toPhoneNumber: "+447955585777",
+			text: "this is test message",
+			gatewayQuality: result.client.gatewayQualityHigh,
+		}, function(_err, _resutl) {
+			
+		});
+	}
+}, true);
