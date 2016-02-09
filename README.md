7824215e38fb5b62dfdc2ba84f3a246496fc1212
Marindrew <a@fj.by>
2016-02-09 14:56:59 +0000

    Added Readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # agile-telecom-sms-node-js
 
+This is library for sending SMS through Agile Telecom API.
+
+Get started:
+```
+var AgileClient = require("./agileClient");
+
+var agileClient = new AgileClient("test", "test", function(err, result) {
+	if(!err && result && result.client) {
+		result.client.sendSms({
+			toPhoneNumber: "+447955585777",
+			text: "Hellowold"
+		}, function(_err, _result) {
+			console.log(_err, _result);
+		});
+	}
+});
+```
--- test.js
+++ test.js
@@ -1,17 +1,26 @@
-
-
 var AgileClient = require("./agileClient");
 
-
-var agileClient = new AgileClient("test", "test", function(err, result) {
+/*var agileClient = new AgileClient("test", "test", function(err, result) {
 	if(!err && result && result.client) {
 		result.client.sendSms({
 			fromPhoneNumber: "Hiboo",
 			toPhoneNumber: "+447955585777",
 			text: "this is test message",
 			gatewayQuality: result.client.gatewayQualityHigh,
-		}, function(_err, _resutl) {
+		}, function(_err, _result) {
 			
 		});
 	}
-}, true);
+}, true);*/
+
+
+var agileClient = new AgileClient("test", "test", function(err, result) {
+	if(!err && result && result.client) {
+		result.client.sendSms({
+			toPhoneNumber: "+447955585777",
+			text: "Hellowold"
+		}, function(_err, _result) {
+			console.log(_err, _result);
+		});
+	}
+});
