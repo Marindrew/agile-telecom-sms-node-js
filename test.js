fe6cefa14cb300940ba56995c3a2a8c415dd9fd3
Marindrew <a@fj.by>
2016-02-09 15:03:40 +0000

    yo

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var AgileClient = require("./agileClient");
 
-/*var agileClient = new AgileClient("test", "test", function(err, result) {
+var agileClient = new AgileClient("test", "test", function(err, result) {
 	if(!err && result && result.client) {
 		result.client.sendSms({
 			fromPhoneNumber: "Hiboo",
@@ -11,16 +11,4 @@ var AgileClient = require("./agileClient");
 			
 		});
 	}
-}, true);*/
-
-
-var agileClient = new AgileClient("test", "test", function(err, result) {
-	if(!err && result && result.client) {
-		result.client.sendSms({
-			toPhoneNumber: "+447955585777",
-			text: "Hellowold"
-		}, function(_err, _result) {
-			console.log(_err, _result);
-		});
-	}
-});
+}, true);
